eac7b5569ebb08f26c84ce70ae795479b857ae5a
k20025291 <mohamed-amine.kenzeddine@etu.univ-amu.fr>
2024-05-25 20:36:51 +0200

    exemple.sh

--- exemple.sh
+++ exemple.sh
@@ -1,3 +1,28 @@
+#!/bin/sh
+
+# Nom de la tâche
+#SBATCH --job-name=ernie_example
+
+# Temps maximum d'exécution (ici 100 heures)
+#SBATCH --time=100:00:00
+
+# Partition (ou queue) à utiliser
+#SBATCH -p kepler
+
+# Nombre de GPU à utiliser
+#SBATCH --gres=gpu:3
+
+# Fichiers de sortie et d'erreur
+#SBATCH --output=./out_ernie_example.txt
+#SBATCH --error=./err_ernie_example.txt
+
+# Notifications par email
+#SBATCH --mail-type=ALL # (BEGIN, END, FAIL or ALL)
+#SBATCH --mail-user=mohamed-amine.kenzeddine@etu.univ-amu.fr
+
+
+
+
 # Exécution du script Python
 # Download Wikidump
   wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
